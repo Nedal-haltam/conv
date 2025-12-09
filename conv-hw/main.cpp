970fdfaa0193f82800d55384aad25002c73e4dca
Nedal-Haltam <nedalhaltam1@gmail.com>
2025-12-09 22:52:50 +0800

    littel refactor

--- conv-hw/main.cpp
+++ conv-hw/main.cpp
@@ -11,7 +11,7 @@
 
 void *pcie_lib_handle;
 PCIE_HANDLE hPCIE;
-const PCIE_LOCAL_ADDRESS LocalAddr = DEMO_PCIE_MEM_ADDR;
+const PCIE_LOCAL_ADDRESS start_address = DEMO_PCIE_MEM_ADDR;
 
 void PCIe_Prologue()
 {
@@ -33,18 +33,18 @@ void PCIe_Epilogue()
 	PCIE_Unload(pcie_lib_handle);
 }
 
-void PCIeWrite(void* data, uint32_t len)
+void PCIeWrite(void* data, PCIE_LOCAL_ADDRESS address, uint32_t len)
 {
-	if (!PCIE_DmaWrite(hPCIE, LocalAddr, data, len))
+	if (!PCIE_DmaWrite(hPCIE, address, data, len))
 	{
 		printf("DMA Memory:PCIE_DmaWrite failed\n");
 		PCIe_Epilogue();
 		exit(1);
 	}
 }
-void PCIeRead(void* data, uint32_t len)
+void PCIeRead(void* data, PCIE_LOCAL_ADDRESS address, uint32_t len)
 {
-	if (!PCIE_DmaRead(hPCIE, LocalAddr, data, len)) {
+	if (!PCIE_DmaRead(hPCIE, address, data, len)) {
 		printf("DMA Memory:PCIE_DmaRead failed\n");
 		PCIe_Epilogue();
 		exit(1);
@@ -54,18 +54,26 @@ void PCIeRead(void* data, uint32_t len)
 void test_pcie()
 {
 	PCIe_Prologue();
+	const bool check = 1;
+	const bool print = 0;
+	const int n = MEM_SIZE;
+	uint8_t writebuff[n] = {0};
+	uint8_t readbuff[n] = {0};
 
-	const int n = 8;
-	u_char writebuff[n] = {0};
-	u_char readbuff[n] = {0};
+	for (int i = 0; i < n; i++) *(writebuff + i) = i + 1;
 
-	for (int i = 0; i < n; i++) *(writebuff + i) = i & 0xFF;
-
-	PCIeWrite(writebuff, n);
-	PCIeRead(readbuff, n);
+	PCIeWrite(writebuff, 0, n * sizeof(writebuff[0]));
+	PCIeRead(readbuff, 0, n * sizeof(readbuff[0]));
 
 	for (int i = 0; i < n; i++) {
-		printf("written=%d read=%d\n", *(writebuff + i), *(readbuff + i));
+		if (print) printf("written=%d read=%d\n", *(writebuff + i), *(readbuff + i));
+		if (check && *(writebuff + i) != *(readbuff + i))
+		{
+			printf("written=%d read=%d\n", *(writebuff + i), *(readbuff + i));
+			printf("address(bytes): %d\n", i);
+			printf("not matched\n");
+			break;
+		}
 	}
 
 	PCIe_Epilogue();
