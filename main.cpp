54f533f66ed153f9ff313a026a0ede359ca981c2
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-12-12 15:48:38 +0300

    add conv2d simple test

--- main.cpp
+++ main.cpp
@@ -764,6 +764,55 @@ void usage(const char* prog_name)
     std::cout << "Supported input/output formats: .ppm, .png, .mp4\n";
 }
 
+void test_conv2d()
+{
+    const int w = 5, h = 5;
+    int in[h][w];
+    int k[KERNEL_HEIGHT][KERNEL_WIDTH] = {
+        {0, 1, 0},
+        {1, 1, 1},
+        {0, 1, 0},
+    };
+
+    int out[h][w];
+    memset(out, 0, sizeof(int) * w * h);
+    for (int i = 0; i < h; i++)
+        for (int j = 0; j < w; j++)
+            in[i][j] = i * w + j + 1;
+    std::cout << "in: \n";
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        std::cout << in[i][j] << " ";
+        std::cout << "\n";
+    }
+    std::cout << "out: \n";
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+            std::cout << out[i][j] << " ";
+        std::cout << "\n";
+    }
+
+
+    conv2d(&in[0][0], k, &out[0][0], w, h, 0, 0, 1, 0);
+
+    std::cout << "in: \n";
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        std::cout << in[i][j] << " ";
+        std::cout << "\n";
+    }
+    std::cout << "out: \n";
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+            std::cout << out[i][j] << " ";
+        std::cout << "\n";
+    }
+}
+
 int main(int argc, char* argv[])
 {
     const char* input_path = NULL;
